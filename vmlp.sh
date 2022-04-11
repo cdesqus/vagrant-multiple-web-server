1303e53e70ca67b8fd92dfca1c2a0a5f73469441
cdesqus <cdesqus@gmail.com>
2022-04-12 05:38:42 +0700

    change-config2

--- vmlp.sh
+++ vmlp.sh
@@ -8,7 +8,7 @@
 
 #update package & Install Package
 sudo apt-get update -y
-sudo apt-get install apache2 php php-mysql -y
+sudo apt-get install apache2 php -y
 sudo apt-get install git -y
 
 # Clone from repository
@@ -20,23 +20,5 @@ sudo rm /var/www/html/index.html
 cd landingpage
 sudo cp -r ./* /var/www/html/
 
-#install mysql-server
-#apt-get install mysql-server -y
-
-#preset answer mysql
-#debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
-#debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
-
-#mysql -u root -e "create user '$DBUSER'@'localhost' identified by '$DBPASSWD'"
-#mysql -u root -e "CREATE DATABASE $DBNAME"
-#mysql -u root -e "grant all privileges on $DBNAME.* to 
-#'$DBUSER'@'%' identified by '$DBPASSWD'"
-
-#allow remote sql from 0.0.0.0
-#sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-#sudo service mysql restart
-
-
 cd /var/www/html/
-#sudo mysql -u root $DBNAME < dump.sql
 sudo service apache2 restart
